11efb3dde14ffb93d401b2f1d3220e1227b1b187
Patricio Perpetua <patoperpetua@singletonsd.com>
2020-11-23 12:16:23 +1000

    Added gitignore. Change logs destination.

--- project/server/logger.js
+++ project/server/logger.js
@@ -9,8 +9,8 @@ const logger = createLogger({
   defaultMeta: { service: 'user-service' },
   transports: [
     new transports.Console(),
-    new transports.File({ filename: 'error.log', level: 'error', timestamp: true }),
-    new transports.File({ filename: 'combined.log', timestamp: true }),
+    new transports.File({ filename: 'logs/error.log', level: 'error', timestamp: true }),
+    new transports.File({ filename: 'logs/combined.log', timestamp: true }),
   ],
 });
 
